3bceee6aad9acbabf181bb71b8d1ec09411226a3
Hakimi Hamzan <hakimihamzan20@gmail.com>
2021-05-02 15:05:56 +0800

    bottom app bar stuff

---
+++ app/src/main/java/xyz/fitforchange/changes/FAB.kt
@@ -0,0 +1,11 @@
+package xyz.fitforchange.changes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FAB : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_f_a_b)
+    }
+}
